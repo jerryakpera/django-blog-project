0f929fef432bf26e0bbd78f0f73399d7570328d3
Jeremiah Akpera <jerryakpera@gmail.com>
2023-11-26 15:53:31 +0100

    blog post model

--- blogger/blogger/blog/models.py
+++ blogger/blogger/blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True, max_length=250)
+
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="post_author"
+    )
+    content = models.TextField()
+
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    STATUS_CHOICES = [
+        ("draft", "Draft"),
+        ("published", "Published"),
+    ]
+
+    status = models.CharField(choices=STATUS_CHOICES, default="draft", max_length=10)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ("-created_at",)
