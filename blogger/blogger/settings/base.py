ff1789988c72ec0d558c6b32b91f1e69ed0d6963
Jeremiah Akpera <jerryakpera@gmail.com>
2023-11-27 07:23:18 +0100

    Scroll pagination

--- blogger/blogger/settings/base.py
+++ blogger/blogger/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blogger.blog",
+    "django_htmx",
 ]
 
 MIDDLEWARE = [
@@ -41,6 +42,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = "blogger.urls"
