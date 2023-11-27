69cdeb681db055e6c8a26c307b0cd5cd31ab5183
Jeremiah Akpera <jerryakpera@gmail.com>
2023-11-27 06:32:32 +0100

    Post card

--- blogger/blogger/blog/views.py
+++ blogger/blogger/blog/views.py
@@ -6,4 +6,9 @@ from .models import Post
 
 class HomeView(ListView):
     model = Post
+
+    # The name of the template to use for the list view
     template_name = "blog/index.html"
+
+    # Name the data that has been queried
+    context_object_name = "posts"
